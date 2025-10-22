b5444e7050f2938a3f2477e8da23d6d299e937ad
Shakil Ahmed <itsdevelopershakil@gmail.com>
2025-10-23 00:04:36 +0600

    about and service section responsive done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,19 +15,19 @@ export default function Home() {
       <section className="pt-[120px]" id="about">
         <div className="max-width">
           <div className="flex mb-5">
-            <span className="block px-5 py-2.5 rounded-[25px] bg-[#F6F7FF] border border-[#686DFD] !font-bricolage">
+            <span className="block px-5 py-2.5 rounded-[25px] text-sm sm:text-base bg-[#F6F7FF] border border-[#686DFD] !font-bricolage">
               NICE TO MEET YOU!
             </span>
           </div>
-          <div className="grid gap-10 grid-cols-12 mb-[60px]">
-            <div className="col-span-7">
-              <h2 className="text-[40px] font-semibold leading-[52px] text-[#101015]">
+          <div className="grid gap-10 md:grid-cols-12 mb-[60px]">
+            <div className="md:col-span-7">
+              <h2 className="text-2xl leading-[36px] sm:text-[40px] font-semibold sm:leading-[52px] text-[#101015]">
                 I help startups make effective digital marketing decisions at
                 every stage of the product life cycle, from introduction to the
                 growth phase.
               </h2>
             </div>
-            <div className="col-span-5">
+            <div className="md:col-span-5">
               <p className="!font-bricolage text-lg leading-[28px] text-[#454545] mb-[30px]">
                 I&apos;m a results-driven Digital Marketing Specialist with 1
                 year of experience helping brands grow through targeted Facebook
@@ -38,15 +38,15 @@ export default function Home() {
               </p>
               <Link
                 href={"#"}
-                className="text-lg text-white font-medium px-8 py-4 rounded-[30px] bg-[#101015] border border-[#101015] hover:bg-transparent hover:text-[#101015] duration-300"
+                className="text-sm sm:text-lg text-white font-medium px-8 py-4 rounded-[30px] bg-[#101015] border border-[#101015] hover:bg-transparent hover:text-[#101015] duration-300"
               >
                 Download CV
               </Link>
             </div>
           </div>
-          <div className="px-[30px] py-10 rounded-[20px] bg-[#F7F7F7] flex justify-between gap-[30px]">
-            <div>
-              <h2 className="text-[22px] font-semibold text-[#101015] mb-[15px]">
+          <div className="px-[30px] py-10 rounded-[20px] bg-[#F7F7F7] flex flex-wrap md:flex-nowrap justify-between gap-[30px]">
+            <div className="w-full md:w-auto">
+              <h2 className="text-lg sm:text-[22px] font-semibold text-[#101015] mb-[15px]">
                 Understanding client needs
               </h2>
               <p className="text-[#454545] leading-[24px]">
@@ -54,11 +54,11 @@ export default function Home() {
                 audience behavior, and market trends.
               </p>
             </div>
-            <div className="w-px h-[100px] rounded-[2px] bg-[#B5B5B5] flex mt-2">
-              <div className="w-full h-1/2 bg-[#454545] rounded-[2px]"></div>
+            <div className="w-full md:w-px h-px md:h-[100px] rounded-[2px] bg-[#B5B5B5] flex mt-2">
+              <div className="w-1/2 md:w-full h-full md:h-1/2 bg-[#454545] rounded-[2px]"></div>
             </div>
-            <div>
-              <h2 className="text-[22px] font-semibold text-[#101015] mb-[15px]">
+            <div className="w-full md:w-auto">
+              <h2 className="text-lg sm:text-[22px] font-semibold text-[#101015] mb-[15px]">
                 Quality and attention to detail
               </h2>
               <p className="text-[#454545] leading-[24px]">
@@ -66,11 +66,11 @@ export default function Home() {
                 to maximize ROI across platforms.
               </p>
             </div>
-            <div className="w-px h-[100px] rounded-[2px] bg-[#B5B5B5] flex mt-2">
-              <div className="w-full h-1/2 bg-[#454545] rounded-[2px] mt-auto"></div>
+            <div className="w-full md:w-px h-px md:h-[100px] rounded-[2px] bg-[#B5B5B5] flex mt-2">
+              <div className="w-1/2 md:w-full h-full md:h-1/2 bg-[#454545] rounded-[2px] ml-auto md:ml-0 md:mt-auto"></div>
             </div>
-            <div>
-              <h2 className="text-[22px] font-semibold text-[#101015] mb-[15px]">
+            <div className="w-full md:w-auto">
+              <h2 className="text-lg sm:text-[22px] font-semibold text-[#101015] mb-[15px]">
                 Innovative digital design solutions
               </h2>
               <p className="text-[#454545] leading-[24px]">
@@ -86,14 +86,14 @@ export default function Home() {
         id="services"
       >
         <div className="max-width">
-          <div className="flex items-start gap-10">
-            <div className="w-1/2">
+          <div className="flex flex-wrap md:flex-nowrap items-start md:gap-10">
+            <div className="w-full md:w-1/2">
               <div className="flex mb-5">
-                <span className="px-5 py-2.5 border border-[#484848] bg-[#1E1E1E] rounded-[25px] !font-bricolage">
+                <span className="px-5 py-2.5 text-sm sm:text-base border border-[#484848] bg-[#1E1E1E] rounded-[25px] !font-bricolage">
                   WHAT I DO
                 </span>
               </div>
-              <h1 className="font-semibold text-5xl leading-[64px]">
+              <h1 className="font-semibold text-2xl sm:text-3xl md:text-5xl leading-[64px]">
                 Industries
               </h1>
               <p className="text-[#B7B7B5] leading-[24px] mt-[15px] mb-[30px]">
@@ -105,12 +105,12 @@ export default function Home() {
               </p>
               <Link
                 href={"#"}
-                className="rounded-[30px] bg-white border border-[#5F5C58] hover:bg-[#5F5C58] hover:text-white duration-300 px-8 py-4 font-medium text-lg text-[#101015] inline-block"
+                className="rounded-[30px] bg-white border border-[#5F5C58] hover:bg-[#5F5C58] hover:text-white duration-300 px-8 py-2.5 sm:py-4 font-medium text-sm sm:text-lg text-[#101015] inline-block"
               >
                 Let&apos;s Talk a Project
               </Link>
             </div>
-            <div className="w-1/2">
+            <div className="w-full md:w-1/2">
               {servicesData.map(({ desc, id, name, tags }) => (
                 <ServiceCard
                   desc={desc}
--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -10,9 +10,11 @@ const ServiceCard = ({
   tags: string[];
 }) => {
   return (
-    <div className="mt-[60px]">
+    <div className="mt-10 sm:mt-[60px]">
       <div className="flex items-center justify-between">
-        <h2 className="text-[32px] leading-[40px] font-semibold">{name}</h2>
+        <h2 className="text-lg sm:text-2xl md:text-[32px] leading-[24px] sm:leading-[32px] md:leading-[40px] font-semibold">
+          {name}
+        </h2>
         <span className="!font-bricolage font-semibold text-[32px] leading-[40px] text-transparent text-stroke">
           {id}
         </span>
@@ -23,7 +25,7 @@ const ServiceCard = ({
         {tags.map((tag, i) => (
           <button
             key={i}
-            className="rounded-[25px] bg-[#1E1E1E] border border-[#484848] px-5 py-2.5 text-[#B7B7B5] leading-[20px]"
+            className="rounded-[25px] bg-[#1E1E1E] border border-[#484848] px-4 sm:px-5 py-2 sm:py-2.5 text-sm sm:text-base text-[#B7B7B5] leading-[20px]"
           >
             {tag}
           </button>
